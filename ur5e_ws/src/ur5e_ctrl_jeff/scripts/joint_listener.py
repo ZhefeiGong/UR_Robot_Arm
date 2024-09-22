9ffc9b75ec1c4fe153b97f301e969951d823eecc
zhefeigong <zhefeigong@gmail.com>
2024-09-22 20:06:12 +0800

    add joint listener

--- ur5e_ws/src/ur5e_ctrl_jeff/scripts/joint_listener.py
+++ ur5e_ws/src/ur5e_ctrl_jeff/scripts/joint_listener.py
@@ -13,5 +13,59 @@ if sys.version_info[0] < 3:
     input = raw_input
 
 class JointStateListener():
-    """"""
+    """Subscribe to the joint states of the UR5e robot"""
+    
+    def __init__(self, verbose: bool = False):
+        self.is_verbose = verbose
+        self.joint_positions = []
+        self.joint_velocities = []
+        self.joint_efforts = []
+        self.joint_names = []
+        
+        # Subscribe to the /joint_states topic
+        rospy.Subscriber("/joint_states", JointState, self.joint_state_callback)
+    
+    def joint_state_callback(self, msg):
+        """Callback function for joint state messages"""
+        
+        # Store joint information
+        self.joint_positions = msg.position
+        self.joint_velocities = msg.velocity
+        self.joint_efforts = msg.effort
+        self.joint_names = msg.name
+
+        # If verbose, print joint information
+        if self.is_verbose:
+            print("[MSG] Joint Names: ", self.joint_names)
+            print("[MSG] Joint Positions: ", self.joint_positions)
+            print("[MSG] Joint Velocities: ", self.joint_velocities)
+            print("[MSG] Joint Efforts: ", self.joint_efforts)
+    
+    def get_joint_states(self):
+        """Get the current joint states (positions, velocities, efforts)"""
+        return {
+            'names': self.joint_names,
+            'positions': self.joint_positions,
+            'velocities': self.joint_velocities,
+            'efforts': self.joint_efforts
+        }
+
+
+def main():
+    """Main Function"""
+
+    print("===== JointStateListener =====")
+    
+    # Initialize the ROS node
+    rospy.init_node('joint_state_listener')
+
+    # Create an instance of the JointStateListener class
+    listener = JointStateListener(verbose=True)
+    
+    # Keep the node running to continuously receive joint state updates
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
 
